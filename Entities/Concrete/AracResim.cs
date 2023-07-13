62b0cd7340a200aca7c6119883cfc0b19e613f78
Can Cakar <54742785+cancakar35@users.noreply.github.com>
2023-07-13 12:17:54 +0300

    Add AracResim entity.

---
+++ Entities/Concrete/AracResim.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class AracResim : IEntity
+    {
+        public int Id { get; set; }
+        public int AracId { get; set; }
+        public string? ImagePath { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
