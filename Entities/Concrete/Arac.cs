4c6077e81c8c79e8d140f234cc1d99ff92ec4f2e
Can Cakar <54742785+cancakar35@users.noreply.github.com>
2023-07-13 11:25:42 +0300

    Add new entities for sql server tables

---
+++ Entities/Concrete/Arac.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Arac : IEntity
+    {
+        public int Id { get; set; }
+        public int? MarkaId { get; set; }
+        public string? Model { get; set; }
+        public string? Seri { get; set; }
+        public Int16? UretimYili { get; set; }
+        public int? RenkId { get; set; }
+        public Int16? MotorGucuHP { get; set; }
+        public int MotorHacmiCC { get; set; }
+        public int KiloMetre { get; set; }
+    }
+}
---
+++ Entities/Concrete/Ilan.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Ilan : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int AracId { get; set; }
+        public string? TelefonNo { get; set; }
+        public string? Aciklama { get; set; }
+        public DateTime? Tarih { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string Mahalle { get; set; }
+        public bool Durum { get; set; }
+    }
+}
