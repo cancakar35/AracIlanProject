71e75d1a2188eee08d914e2341a498102c1a6055
Can Cakar <54742785+cancakar35@users.noreply.github.com>
2023-08-16 12:09:14 +0300

    Remove swagger

--- AracIlanProject/Program.cs
+++ AracIlanProject/Program.cs
@@ -16,12 +16,9 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 
 builder.Services.AddScoped<IUserDal, EfUserDal>();
@@ -74,12 +71,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseCors(builder=> builder.WithOrigins("https://localhost:4200", "http://localhost:4200").AllowAnyHeader().AllowCredentials());
 
